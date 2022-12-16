9bd63a773eac638d10c583d28015f3f750d1728a
williambjork <william.bjork@gobil.se>
2022-12-16 10:33:36 +0100

    init commit

---
+++ components/Day1.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {day1input} from "../util.js"
+
+export default function Day1() {
+  return (
+    <div>
+        <div>Day1</div>
+        <p>{day1input}</p>
+    </div>
+  )
+}
+
